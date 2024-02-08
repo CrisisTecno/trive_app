312c15a077f158ada328fea42b38dc945f27b1bf
BEREKAST <beymar_67@hotmail.com>
2024-02-07 22:04:41 -0400

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:trive_bysc/pages/Splash/encabezado.dart';
 import 'pages/pages.dart';
 import 'utils/utils.dart';
 
-
 void main() {
-    WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
@@ -17,16 +17,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Sizer(
       builder: (context, orientation, deviceType) {
-      return MaterialApp(
-      title: 'Trive',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home:  SplashScreen(),
-    );
-    },
+        return MaterialApp(
+          title: 'Trive',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: Encabezado(),
+        );
+      },
     );
   }
 }
